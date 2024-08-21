e9fbd83a9680a5768b7c65492be35fbdb90f5f1b
Mohan Madhan <mohan@360ithub.com>
2024-08-21 15:13:31 +0530

    first commit

--- user_management/user_manangement/doctype/user_management/user_management.js
+++ user_management/user_manangement/doctype/user_management/user_management.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Pankaj and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("User Management", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("User Management", {
+	refresh(frm) {
+        frm.fields_dict['role'].get_query = function(doc) {
+            return {
+                filters: {
+                    "custom_custom_role": 1
+                }
+            };
+        };
+	},
+});
